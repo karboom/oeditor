9416d14b82b2fe29466a86384389808f68c54c0b
karboom <mail@karboom.me>
2017-02-25 21:52:44 +0800

    refactor: 重构上传相关逻辑

--- oeditor.js
+++ oeditor.js
@@ -3,9 +3,9 @@ const moment = require('moment')
 const fs = require('fs')
 
 
-let upload_check = function *(file, config, action) {
-    let size = config[action + 'MaxSize']
-    let ext = config[action + 'AllowFiles']
+let upload_check = function *(file, config) {
+    let size = config['MaxSize']
+    let ext = config['AllowFiles']
 
     if (file.size > size) {
         throw  `文件大小超出限制(小于${size})`
@@ -49,23 +49,49 @@ let make_pic_success_body = function (url, name) {
     }
 }
 
+let get_config_fields = function (config, action) {
+    let obj = {}
+    for (let key of Object.keys(config)) {
+        if (key.indexOf(action) == 0) {
+            let new_key = key.replace(action, '')
+            obj[new_key] = config[key]
+        }
+    }
+
+    return obj
+}
+
+let file_handle = function*(config, storage) {
+    let field_name = config['FieldName']
+    let file = this.request.body.files[field_name]
+
+    yield upload_check.call(this, file, config)
+    let file_name_tpl = config['PathFormat']
+    let file_path = format_path(file_name_tpl, file.name)
+
+    yield storage.upload(file.path, file_path)
+
+    this.body = make_pic_success_body(file_path, file.name)
+}
+
 let handles = {
-    image: function *(config, storage) {
-        let field_name = config['imageFieldName']
-        let file = this.request.body.files[field_name]
+    image: function *() {
+        yield file_handle.apply(this, arguments)
+    },
 
-        yield upload_check.call(this, file, config, 'image')
-        let file_name_tpl = config['imagePathFormat']
-        let file_path = format_path(file_name_tpl, file.name)
+    scrawl: function*() {
+    },
 
-        file_path = '/qiniu' + file_path
-        yield storage.upload(file.path, file_path)
+    video: function*() {
+        yield file_handle.apply(this, arguments)
+    },
 
-        this.body = make_pic_success_body(file_path, file.name)
+    file: function*() {
+        yield file_handle.apply(this, arguments)
     },
 
     imageManager: function*(config, storage) {
-        let prefix = config['imageManagerListPath'] + config['imageManagerUrlPrefix']
+        let prefix = config['ListPath'] + config['UrlPrefix']
 
         let start = this.query.start
         let size = this.query.size
@@ -78,10 +104,6 @@ let handles = {
             start,
             total: 999
         }
-    },
-
-    config: function *(config) {
-        this.body = config
     }
 }
 
@@ -96,7 +118,9 @@ module.exports = function (opt) {
 
         // 找出匹配的处理器
         if (query.action == 'config') {
-            match = query.action
+            this.body = config
+
+            return
         } else {
             for (let action of Object.keys(handles)) {
                 if (config[action + 'ActionName'] == query.action) {
@@ -107,7 +131,7 @@ module.exports = function (opt) {
 
         if (match) {
             try {
-                yield handles[match].call(this, config, storage)
+                yield handles[match].call(this, get_config_fields(config, match), storage)
             } catch (e) {
                 console.log(e)
                 this.body = {
